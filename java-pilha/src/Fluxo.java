97cdfb2fdd7468c2ef8ec35704bf9e6aaae16013
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2022-10-17 15:59:16 -0300

    throw

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -20,12 +20,20 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            int a = i/0;
-            Conta c = null;
-            c.deposita();
-        }
-        System.out.println("Fim do metodo2");
+//        for(int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//            int a = i/0;
+//            Conta c = null;
+//            c.deposita();
+//        }
+        ArithmeticException ex = new ArithmeticException("deu errado");
+        throw ex;
+        //System.out.println("Fim do metodo2");
     }
+
+// Não é necessário guardar a exceção em uma referência, você pode lançá-la diretamente em uma linha só:
+//    private static void metodo2() {
+//        System.out.println("Ini do metodo2");
+//        throw new ArithmeticException();
+//    }
 }
